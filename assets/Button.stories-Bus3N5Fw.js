93532cf51a1365ccf758b9ac9cd37708b544d6ee
rreyescc <rreyescc@users.noreply.github.com>
2024-08-19 20:33:06 +0000

    deploy: fa650c72ca122fef11f34bd1af8cbea468a83b36

--- assets/Button.stories-Bus3N5Fw.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const s=({text:n})=>a.jsx("button",{type:"button",children:n});s.__docgenInfo={description:"",methods:[],displayName:"Button",props:{text:{required:!0,tsType:{name:"string"},description:""}}};const m={title:"Design System/Atoms/Button",component:s},t={args:{text:"default"}};var e,r,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    text: 'default'
-  }
-}`,...(o=(r=t.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const c=["Default"];export{t as Default,c as __namedExportsOrder,m as default};
