8efce2a63261cbcb88978ee8e80105dae1a438d5
rreyescc <rreyescc@users.noreply.github.com>
2024-08-19 06:16:34 +0000

    deploy: c0d8d556d1b96e577410f2130209397369452c8d

---
+++ assets/Label.stories-BMIfCOvj.js
@@ -0,0 +1,13 @@
+import{j as n}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const s=({text:t,color:o="base"})=>n.jsx("label",{className:`label label--color-${o}`,children:t});s.__docgenInfo={description:"",methods:[],displayName:"Label",props:{text:{required:!0,tsType:{name:"string"},description:""},color:{required:!1,tsType:{name:"union",raw:"'primary' | 'secondary' | 'tertiary' | 'success' | 'warning' | 'danger' | 'info' | 'base'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'tertiary'"},{name:"literal",value:"'success'"},{name:"literal",value:"'warning'"},{name:"literal",value:"'danger'"},{name:"literal",value:"'info'"},{name:"literal",value:"'base'"}]},description:"",defaultValue:{value:"'base'",computed:!1}}}};const c={title:"Design System/Atoms/Label",argTypes:{},parameters:{layout:"centered"},tags:["autodocs"],component:s},e={args:{color:"primary",text:"label"}};var a,r,l;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    color: 'primary',
+    text: 'label'
+  }
+  // render: (args) => (
+  //   <>
+  //     <Label {...args }></Label>
+  //     <Label {...args } color="secondary"></Label>
+  //     <Label {...args } color="tertiary"></Label>
+  //   </>
+  // )
+}`,...(l=(r=e.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const u=["Primary"];export{e as Primary,u as __namedExportsOrder,c as default};
