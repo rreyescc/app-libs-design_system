17f44722d2f7d7c0d2f0239dabe1b20239c08b3e
rreyescc <rreyescc@users.noreply.github.com>
2024-08-19 21:24:32 +0000

    deploy: d2a638596fbf0480eccd916e3fb18c54b61e3978

---
+++ assets/Button.stories-wpJ3KkMb.js
@@ -0,0 +1,5 @@
+import{j as a}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const r=({text:n})=>a.jsx("button",{type:"button",className:"button",children:n});r.__docgenInfo={description:"",methods:[],displayName:"Button",props:{text:{required:!0,tsType:{name:"string"},description:""}}};const p={title:"Design System/Atoms/Button",component:r},t={args:{text:"default"}};var e,s,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    text: 'default'
+  }
+}`,...(o=(s=t.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const c=["Default"];export{t as Default,c as __namedExportsOrder,p as default};
